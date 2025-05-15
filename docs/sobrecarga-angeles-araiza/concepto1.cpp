8c648f026063d2a384872086c4dcc3cc013ac948
Angeles Araiza <a00574806@tec.mx>
2025-05-15 15:29:32 -0600

    Update concepto1.cpp

--- docs/sobrecarga-angeles-araiza/concepto1.cpp
+++ docs/sobrecarga-angeles-araiza/concepto1.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+
+class Complejo{
+    public:
+    double re, im;
+    Complejo(double re, double im):re(re), im(im) {}
+    Complejo operator+(const Complejo& otro){
+        return Complejo(re + otro.re, im+ otro.im);
+    }
+};
+
+int main() {
+    Complejo a(1.0,2.0), b(3.0,-1.0);
+    Complejo resultado= a+b;
+    cout<<"Resultado ("<< resultado.re<<","<<resultado.im<<")"<<endl;
+    return 0;
+}
